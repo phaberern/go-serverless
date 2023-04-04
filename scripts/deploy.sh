f48edd0fe73fcddabebbd4a36268808f220768bb
Phil <phillip.haberern5@gmail.com>
2023-04-03 20:28:16 -0400

    fix:bash script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
-#!bin/bash
-for dir in ./functions/*
+#!/bin/bash
+for dir in ../functions/*
 do
-    function_name="${basename $dir}"
-    function_directory="./functions/${function_name}"
-    binary_file="./bin/${function_name}"
-    zip_file="./bin/${function_name}.zip"
+    function_name="$(basename $dir)"  
+    function_directory="$dir"  
+    binary_file="../bin/${function_name}"
+    zip_file="../bin/${function_name}.zip"
 
     echo "building $function_name..."
     GOARCH=amd64 GOOS=linux go build -ldflags="-s -w" -o $binary_file $function_directory
